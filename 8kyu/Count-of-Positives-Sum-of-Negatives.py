91cb14ced653464a627bd2e61abb0f1fbe65a89c
Julia Torrejon <juliaesther.torrejon@gmail.com>
2018-12-01 18:29:01 +0000

    Update Count-of-Positives-Sum-of-Negatives.py

--- 8kyu/Count-of-Positives-Sum-of-Negatives.py
+++ 8kyu/Count-of-Positives-Sum-of-Negatives.py
@@ -1,11 +1,11 @@
 def count_positives_sum_negatives(arr):
     if not arr: return []
-    positive = 0
-    negative = 0
-    for x in arr:
-      if x > 0:
+    count_positives = 0
+    count_negatives = 0
+    for n in arr:
+      if n > 0:
           positive += 1
-      if x < 0:
-          negative += x
-    return [positive, negative]
+      if n < 0:
+          negative += n
+    return [count_positives, sum_negatives]
     
