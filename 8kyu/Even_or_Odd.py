04ec463b076e83c4368874ba465dea33991af02e
Julia Torrejon <juliaesther.torrejon@gmail.com>
2018-12-02 16:27:46 +0000

    Create Even_or_Odd.py

---
+++ 8kyu/Even_or_Odd.py
@@ -0,0 +1,12 @@
+def even_or_odd(number):
+  return 'Odd' if number % 2 else 'Even'
+  
+# Notes: The % (modulo) operator yields the remainder from the division of the first argument by the second. An expression like x % y evaluates to the remainder of x ÷ y. Precedence is the same as operators / (division) and * (multiplication).
+
+>>> 9 / 2
+4
+>>> 9 % 2
+1
+9 divided by 2 is equal to 4.
+4 times 2 is 8
+9 minus 8 is 1 - the remainder.
